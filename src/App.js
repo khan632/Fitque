9c5760a7b5d4e0fedb8177d6efbdaee7da060d57
md areeb uddin <mdareeb.uddin@truelancer.com>
2023-02-24 15:56:54 +0530

    Add some logic and Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import './App.css'
+import Navbar from './Component/Navbar';
+import Footer from './Component/Footer';
+import Home from './Pages/Home';
+import ExerciseDetail from './Pages/ExerciseDetail';
 
 function App() {
   return (
-    <div className="">Areeb</div>
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m='auto'>
+
+      <Navbar />
+      <Routes>
+        <Route path='/' element={ <Home />} />
+        <Route path='/exercise/:id' element={ <ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
